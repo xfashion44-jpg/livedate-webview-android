9ed085e785792a169f5dc8a80dfd690854d8be7e
xfashion44-jpg <xfashion44@gmail.com>
2026-03-04 16:44:44 +0900

    Launch MainActivity immediately on call end to avoid delayed page_4 redirect (1.0.38)

--- app/src/main/java/kr/freenote/livedate/CallActivity.java
+++ app/src/main/java/kr/freenote/livedate/CallActivity.java
@@ -1013,14 +1013,19 @@ public class CallActivity extends AppCompatActivity {
         finishing = true;
         logAudioManagerState("hangupRequested_beforeFinish");
         try {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("app_url", RETURN_URL_PAGE4);
+            intent.putExtra("force_load", true);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(intent);
+            Log.i(TAG, ts() + " returnToInbox startMain app_url=" + RETURN_URL_PAGE4 + " taskId=" + getTaskId());
             Intent result = new Intent();
             result.putExtra("return_url", RETURN_URL_PAGE4);
             setResult(RESULT_OK, result);
-            Log.i(TAG, ts() + " returnToInbox setResult return_url=" + RETURN_URL_PAGE4);
         } catch (Exception e) {
-            Log.i(TAG, ts() + " returnToInbox setResult failed=" + e.getClass().getSimpleName());
+            Log.i(TAG, ts() + " returnToInbox startMain/setResult failed=" + e.getClass().getSimpleName());
         }
-        Log.i(TAG, ts() + " returnToInbox finishWithResult=true");
+        Log.i(TAG, ts() + " returnToInbox finishAfterMain=true");
         if (!isFinishing()) finish();
     }
 
